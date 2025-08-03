a76e21805be90e32a7f4410c3f12a6b17fc75df8
abhishek11das <diptabhi206@gmail.com>
2025-08-03 13:37:54 +0600

    Initial setup: Rest Assured API automation for DailyFinance

---
+++ src/test/java/runner/CostTest.java
@@ -0,0 +1,102 @@
+package runner;
+
+import config.Setup;
+import controller.CostController;
+import model.CostModel;
+import com.github.javafaker.Faker;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.Properties;
+
+public class CostTest extends Setup {
+
+    CostController costController = new CostController();
+    Properties props = new Properties();
+    String costId;
+
+    @Test(priority = 1, description = "Add Cost Item")
+    public void addItem() throws IOException {
+        loadProps();
+        String token = props.getProperty("userToken");
+
+        Faker faker = new Faker();
+        CostModel item = new CostModel(
+                faker.commerce().productName(),
+                faker.number().numberBetween(1, 5),
+                String.valueOf(faker.number().numberBetween(100, 999)),
+                "2025-08-02",
+                "May",
+                faker.lorem().word()
+        );
+
+        Response res = costController.addItem(item, token);
+        Assert.assertEquals(res.statusCode(), 201);
+
+        costId = res.jsonPath().get("_id");
+        updateProperty("costId", costId);
+    }
+
+    @Test(priority = 2, description = "Get list of Items")
+    public void getItems() throws IOException {
+        loadProps();
+        String token = props.getProperty("userToken");
+
+        Response res = costController.getItems(token);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
+    @Test(priority = 3, description = "Edit Item Name")
+    public void editItem() throws IOException {
+        loadProps();
+        String token = props.getProperty("userToken");
+        String costId = props.getProperty("costId");
+
+        Faker faker = new Faker();
+        String updateBody = "{\n" +
+                "  \"itemName\": \"" + faker.commerce().productName() + "\"\n" +
+                "}";
+
+        Response res = costController.updateItem(costId, token, updateBody);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
+    @Test(priority = 4, description = "Delete Item with valid Cost Id")
+    public void deleteItem() throws IOException {
+        loadProps();
+        String token = props.getProperty("userToken");
+        String costId = props.getProperty("costId");
+
+        Response res = costController.deleteItem(costId, token);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+    @Test(priority = 5, description = "Try to Delete Item with Invalid Cost Id")
+    public void deleteItemWithwrongCostID() throws IOException {
+        loadProps();
+        String token = props.getProperty("userToken");
+        String costId = props.getProperty("invalidid123");
+        Response res = costController.deleteItem(costId, token);
+        Assert.assertEquals(res.statusCode(), 404);
+        String message = res.jsonPath().getString("message");
+        Assert.assertTrue(message.contains("Cost not found"), "Unexpected error message");
+    }
+
+    private void loadProps() throws IOException {
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        props.load(fis);
+        fis.close();
+    }
+
+    private void updateProperty(String key, String value) throws IOException {
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        props.load(fis);
+        fis.close();
+
+        props.setProperty(key, value);
+        FileOutputStream fos = new FileOutputStream("src/test/resources/config.properties");
+        props.store(fos, null);
+        fos.close();
+    }
+}
---
+++ src/test/java/runner/UserTest.java
@@ -0,0 +1,70 @@
+package runner;
+
+import com.github.javafaker.Faker;
+import config.Setup;
+import controller.UserController;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class UserTest extends Setup {
+
+    UserController userController = new UserController();
+    Properties props = new Properties();
+
+    @Test(priority = 1, description = "Get User List")
+    public void getUserList() throws IOException {
+        loadProps();
+        String token = props.getProperty("adminToken");
+
+        Response res = userController.getUserList(token);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
+    @Test(priority = 2, description = "Search by Valid User Id")
+    public void searchUserById() throws IOException {
+        loadProps();
+        String token = props.getProperty("adminToken");
+        String userId = props.getProperty("userId");
+
+        Response res = userController.getUserById(userId, token);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+    @Test(priority = 3, description = "Search by Invalid User Id")
+    public void searchUserByInvalidId() throws IOException {
+        loadProps();
+        String token = props.getProperty("adminToken");
+        String userId = props.getProperty("invalidId123");
+
+        Response res = userController.getUserById(userId, token);
+        Assert.assertEquals(res.statusCode(), 404);
+        String message = res.jsonPath().getString("message");
+        Assert.assertTrue(message.contains("User not found"), "Unexpected error message");
+    }
+    @Test(priority = 4, description = "Edit User Info")
+    public void editUserInfo() throws IOException {
+        loadProps();
+        String token = props.getProperty("adminToken");
+        String userId = props.getProperty("userId");
+        Faker faker = new Faker();
+
+        String updateBody = "{\n" +
+                "  \"firstName\": \"" + faker.name().firstName() + "\",\n" +
+                "  \"lastName\": \"" + faker.name().lastName() + "\",\n" +
+                "  \"phoneNumber\": \"" + faker.phoneNumber().subscriberNumber(11) + "\",\n" +
+                "  \"address\": \"" + faker.address().fullAddress() + "\"\n" +
+                "}";
+        Response res = userController.updateUser(userId, token, updateBody);
+        Assert.assertEquals(res.statusCode(), 200);
+    }
+
+    private void loadProps() throws IOException {
+        FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+        props.load(fis);
+        fis.close();
+    }
+}
